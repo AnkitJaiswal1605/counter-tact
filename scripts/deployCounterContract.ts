847b80ff4ca252f60bef9e50baa84687c8592f01
vansh tyagi <113906488+vanshtyagi38@users.noreply.github.com>
2023-11-02 22:58:14 +0530

    Update deployCounterContract.ts
    Now it will be more effective and faster : error handling also added for better reliability

--- scripts/deployCounterContract.ts
+++ scripts/deployCounterContract.ts
@@ -2,23 +2,33 @@ import { toNano } from 'ton-core';
 import { CounterContract } from '../wrappers/CounterContract';
 import { NetworkProvider } from '@ton-community/blueprint';
 
-export async function run(provider: NetworkProvider) {
-    const counterContract = provider.open(await CounterContract.fromInit(36427n));
+async function deployCounterContract(provider: NetworkProvider, valueInTON: number, queryId: bigint = 0n) {
+    try {
+        const contractId = 36427n; // Replace with the actual contract ID
 
-    await counterContract.send(
-        provider.sender(),
-        {
-            value: toNano('0.05'),
-        },
-        {
-            $$type: 'Deploy',
-            queryId: 0n,
-        }
-    );
+        const counterContract = await CounterContract.fromInit(contractId);
+        const senderAddress = provider.sender();
+        const nanoValue = toNano(valueInTON);
 
-    await provider.waitForDeploy(counterContract.address);
+        await counterContract.send(senderAddress, { value: nanoValue }, { $$type: 'Deploy', queryId });
 
-    // run methods on `counterContract`
+        await provider.waitForDeploy(counterContract.address);
+
+        return counterContract;
+    } catch (error) {
+        console.error('Error deploying counter contract:', error);
+        throw error; // Re-throw the error for handling at a higher level
+    }
 }
 
+export async function run(provider: NetworkProvider) {
+    try {
+        const counterContract = await deployCounterContract(provider, 0.05);
 
+        // Run methods on `counterContract`
+
+    } catch (error) {
+        console.error('Error in run function:', error);
+        // Handle the error or log it as needed
+    }
+}
